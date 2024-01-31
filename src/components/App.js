1c47093225ae9130f0355d9c4f70c7bff73f043a
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2024-01-31 14:47:36 +0530

    comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text, setText] = useState("");
+  const [arr, setArr] = useState([])
+
+
+  const fetchSuggestions =  (query) => {
+    const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+    const filteredSuggestions = fruits.filter(suggestion =>
+      suggestion.toLowerCase().includes(query.toLowerCase())
+    );
+    setArr(filteredSuggestions);
+  };
+
+  useEffect(() => {
+    fetchSuggestions(text);
+  }, [text]);
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input type="text" value={text} onChange={(e)=>setText(e.target.value)} />
+        <ul>
+        {
+          arr.map((item)=>{
+            return(
+              <li>{item}</li>
+            )
+          })
+        }
+        </ul>
     </div>
   )
 }
